11776262a48340b2b74fa55a384096655d31348e
kur <rhdnfka94@gmail.com>
2019-07-08 12:29:53 +0900

    Remove useless comments and codes

--- main.py
+++ main.py
@@ -30,7 +30,6 @@ fu.write_text('index.html', document_str([], [
 
 def popup_btn(match_id, content):
     return h('label', class_='btn', for_=match_id)[content]
-#print( popup_btn('open-pop', 'open'))
 def popup_window(match_id, content):
     ''' input after div - order is important! '''
     return [
@@ -43,12 +42,6 @@ def popup_window(match_id, content):
             ],
         ]
     ]
-#print( popup_window('ppap', [p('content')]*2)[1].pretty() ) 
-#print( popup_window('ppap', [p('content')]*2)[1] ) 
-#print( h('input', class_='modal-state', id=match_id, type='checkbox'))
-#print('-----')
-#print( popup_window('open-popup', ['pen',a('pineapple'),p('apple'),'pen']) )
-#print( div(popup_window('open-popup', ['pen',a('pineapple'),p('apple'),'pen'])).pretty() )
 
 #=================================================================
 root_dir = Path(sys.argv[1])
@@ -58,53 +51,9 @@ srcA_paths = fpaths(root_dir / 'Formatted_A')
 srcB_paths = fpaths(root_dir / 'Formatted_B')
 tokA_paths = fpaths(root_dir / 'Token_A')
 tokB_paths = fpaths(root_dir / 'Token_B')
-# 이걸 맵핑(idx:value)으로 써도 되는가? src와 tok의 길이는 동일한게 확실한가?
-# * 길이가 다르면 뭔가 심각한 오류가 발생한 것인가?
+
 assert len(srcA_paths) == len(tokA_paths) 
 assert len(srcB_paths) == len(tokB_paths)
-print(*car_paths, sep='\n',end='\n--------\n')
-print(*srcA_paths,sep='\n',end='\n--------\n')
-print(*tokA_paths,sep='\n',end='\n--------\n')
-print(*srcB_paths,sep='\n',end='\n--------\n')
-print(*tokB_paths,sep='\n',end='\n--------\n')
-    #exit()
-
-#print(*str(BeautifulSoup(src2).find_all('pre')[1]).split('\n'), sep='\n\n')
-#print(':',len(BeautifulSoup(src1).find_all('pre')[1].text.split('\n')))
-'''
-print('---------')
-#fu.write_text('tmp',src1)
-print(*BeautifulSoup(src1).find_all('pre')[1].text.split('\n'),sep='\n')
-print(*src1.split('\n'),sep='\n')
-print(len(src1.split('\n')))
-print('---------')
-#for i in range(2):
-print('---------')
-print(src1.split('</pre>')[0])
-print('---------')
-print(len(src1.split('<pre>')))
-print('---------')
-print(len(src1.split('<pre>',maxsplit=1)))
-#fu.write_text('tmp.html', src1.split('<pre>', maxsplit=1)[1] .split('</pre>',maxsplit=1)[1])
-#print(len(BeautifulSoup(src1).find_all('pre')[1].text.split('\n')))
-#print(src2)
-
-print('---------')
-fu.write_text('tmp.html',str(BeautifulSoup(src2).find_all('pre')[1]))
-#NOTE: You can use below to split span codes into line by line!
-print('---------')
-print(len(str(BeautifulSoup(src2).find_all('pre')[1]).split('\n')))
-
-print(
-    div(class_='modal')[
-        h('label', class_='modal_bg', for_=match_id),
-        div(class_='modal_inner')[
-            [h('label', class_='modal_close', for_=match_id)]
-            +[a('ppap')]
-        ]
-    ].pretty()
-)
-'''
 
 #=================================================================
 highlight_css = 'css/highlight.css'
@@ -125,12 +74,9 @@ table = [
         h('tr')[ h('td')['src1'], h('td')['src2'], h('td')[3],],
         h('tr')[ h('td')['src1'], h('td')['src3'], h('td')[3],],
         h('tr')[ h('td')['src1'], h('td')['src4'], h('td')[3],],
-        #[ h('tr')[ h('td')[1], h('td')[2], h('td')[312],] ] * 100,
     ]),
     popup_btn(match_id, 'view matching'),
     popup_window(match_id, '{match}')
-        #h('pre',class_='highlight')[span('123',class_='cp'),span('214231',class_='cp')])
-        # 2 column table in 2 <pre>,
 ]
 
 #-----------------------------------------------------------------
