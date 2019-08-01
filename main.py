f802e92eb4867df266d4111e5157d8afe3535fb1
kur <rhdnfka94@gmail.com>
2019-08-01 14:30:47 +0900

    [#1] Edit comments in main.py

--- main.py
+++ main.py
@@ -22,17 +22,13 @@ import consts
 import pages.index, pages.overview, pages.comp
 import data
 
-#--------------------------------------------------------------------------------------
-#-----------------------------------------------------------------
-
-#-----------------------------------------------------------------
-#--------------------------------------------------------------------------------------
 def main(args=None):
-    # global constant data
+    # global (constant) data
     gdat = consts.consts(
         args if args else consts.args()
     )
 
+    # Generate index.html
     fu.write_text(
         Path(gdat.OUTPUT_ROOT,'index.html'), 
         pages.index.page(gdat)
@@ -49,35 +45,37 @@ def main(args=None):
         for src,dst in zip(srcs,dsts):
             shutil.copyfile(src,dst)
 
-    #=================================================================
     for TARGET_CAR,OUTPUT_DIR in zip(gdat.TARGET_CARS,gdat.OUTPUT_DIRS):
+        # Copy fixed css,js scripts
         print('Processing on {}...'.format(TARGET_CAR))
         fp.foreach(copy_fixed(OUTPUT_DIR), ['css','js'])
 
+        # Generate code style css
         fu.write_text(
             Path(OUTPUT_DIR, 'css/highlight.css'),
             HtmlFormatter().get_style_defs('.highlight'),
         )
 
-        #-----------------------------------------------------------------
+        # Data processing
         root_dir = Path(gdat.INPUT_DIR)
         read_json = fp.pipe(fu.read_text, json.loads)
         car_dict = read_json(root_dir / 'Alignment' / TARGET_CAR)
-
         comp_data = data.comp_data(gdat, car_dict)
+
+        # Generate overview.html
+        fu.write_text(
+            Path(OUTPUT_DIR,'overview.html'), 
+            pages.overview.page(comp_data)
+        )
+
+        # Generate comp/X_X.html
         html_paths = comp_data.html_paths
         comp_htmls = pages.comp.page(gdat, comp_data)
-        #-----------------------------------------------------------------
         for path,html in tqdm(zip(html_paths, comp_htmls),
                               total=len(html_paths),
                               desc='wrte html to disk'):
             fu.write_text(Path(OUTPUT_DIR,path), html)
 
-        #=================================================================
-        fu.write_text(
-            Path(OUTPUT_DIR,'overview.html'), 
-            pages.overview.page(comp_data)
-        )
 
 if __name__ == '__main__':
     main()
