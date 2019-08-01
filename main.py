1b8985564b06f3dd3455b2b47fad0e632e864026
kur <rhdnfka94@gmail.com>
2019-08-01 12:59:11 +0900

    Extract overview page generation to pages.overview.py(#1)

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from pygments.lexers import CppLexer
 from pygments.formatters import HtmlFormatter
 
 import consts
-import pages.index
+import pages.index, pages.overview
 
 #--------------------------------------------------------------------------------------
 from collections import namedtuple
@@ -398,60 +398,11 @@ def main(args=None):
             fu.write_text(Path(OUTPUT_DIR,path), html)
 
         #=================================================================
-        def match_link(href, content):
-            return h('a',href=href)[content]
-        def link_row(name_pair, match_pair, href, content):
-            a,b = match_pair
-            match_pairs = match_pair_dic[a.fidx, b.fidx]
-            num_matches = len(match_pairs)
-            score_sum = sum(m.abs_score for m,_ in match_pairs)
-            a_name,b_name = name_pair
-            return h('tr')[ 
-                h('td',class_='center_cell')[a_name], 
-                h('td',class_='center_cell')[b_name], 
-                h('td')[num_matches],
-                h('td')[score_sum],
-                h('td')['{:.1f}'.format(score_sum / num_matches)],
-                h('td',class_='center_cell')[match_link(href,content)],
-            ]
 
-        fu.write_text(Path(OUTPUT_DIR,'overview.html'), document_str(
-            [
-                link(rel="stylesheet", href="css/overview.css"),
-                link(rel="stylesheet", href='css/table.css'),
-            ], 
-            [
-                h1('overview', style='text-align: center; font-size: 3em;'),
-                div(class_='row')[
-                    div(class_='column left')[
-                        h2('Result Matrix'),
-                        p('Matrix will be included'),
-                    ],
-                    div(class_='column right')[
-                        h2('Result Overview'),
-                        p('테이블의 헤더를 클릭하여 정렬할 수 있습니다.'),
-
-                        h('table',class_='overview_table')[
-                            h('tr', children=
-                                [h('th',class_='center_cell')[s] for s in [
-                                    'A 파일','B 파일'
-                                ]] + [h('th',s) for s in [
-                                    '매치수','점수총합','평균점수',
-                                ]] + [h('th',class_='center_cell')[
-                                    '비교 화면 보기'
-                                ]]
-                            ),
-                            fp.lmap(
-                                link_row, 
-                                match_name_pairs, unique_match_pairs,
-                                html_paths, html_paths
-                            ),
-                        ],
-                    ],
-                ],
-                h('script', src='js/sort_table.js')[' '],
-            ]
-        ))
+        fu.write_text(Path(OUTPUT_DIR,'overview.html'), 
+            pages.overview.page(
+                match_pair_dic, match_name_pairs, unique_match_pairs, html_paths
+            ))
 
         '''
         #=================================================================
