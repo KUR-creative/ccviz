4d3fa41f1705e158af508458ab435b2119d176ab
kur <rhdnfka94@gmail.com>
2019-08-26 15:10:51 +0900

    Remove test result file first

--- functional_test.py
+++ functional_test.py
@@ -4,20 +4,21 @@ from collections import namedtuple
 import main
 
 def test_no_matches_but_ok():
+    result_fpath = 'no-match-test-result'
+    try:
+        shutil.rmtree(result_fpath)
+    except FileNotFoundError as e:
+        print(e)
+
     Args = namedtuple(
         'Args', 
         'input_zip output_directory absolute_score_threshold relative_score_threshold')
     main.main(Args(
         input_zip=str('fixture/no-match-case/164.125.34.91_2019-08-21-14-27-04.zip'),
-        output_directory=str('tmp'),
+        output_directory=str(result_fpath),
         absolute_score_threshold=100,
         relative_score_threshold=0.5
     ))
-
-    try:
-        shutil.rmtree('tmp')
-    except FileNotFoundError as e:
-        print(e)
 
     print('No crash, then ok. Check buttons in index.html later')
 
