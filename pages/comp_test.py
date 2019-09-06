509b3d5dcecdf845e797559158b07550019c3529
kur <rhdnfka94@gmail.com>
2019-09-06 12:25:29 +0900

    Import functions directly

--- pages/comp_test.py
+++ pages/comp_test.py
@@ -1,25 +1,24 @@
 # map <F5> :wa<CR>:!pytest -vv pages/comp_test.py<CR>
-import pages.comp
+from pages.comp import sync_tok, sync_li2
 
 def test_sync_tok__pad_space():
-    a,b = pages.comp.sync_tok('123', 'abc ')
+    a,b = sync_tok('123', 'abc ')
     assert len(a) == len(b)
     assert a == '123 '
 
 def test_sync_tok__pad_last_newline_chars__if_a_has_last_newline_chars():
-    a,b = pages.comp.sync_tok('123\n\n', '123xa')
+    a,b = sync_tok('123\n\n', '123xa')
     assert len(a) == len(b)
     assert a == '123  \n\n'
     assert b == '123xa\n\n'
 
 def test_sync_tok__pad_last_newline_chars__if_a_has_last_newline_chars2():
-    a,b = pages.comp.sync_tok(' 123-219  \n\n\n', '123xa')
+    a,b = sync_tok(' 123-219  \n\n\n', '123xa')
     assert len(a) == len(b)
     assert a == ' 123-219  \n\n\n'
     assert b == '123xa     \n\n\n'
 
 
-from pages.comp import sync_li2
 def test_sync_li2__len_src_greater_than_len_dst():
     assert sync_li2([1,2,3], [1,2], modify_left=True)  == [2,3]
     assert sync_li2([1,2,3], [1,2], modify_left=False) == [1,2]
@@ -31,3 +30,4 @@ def test_sync_li2__len_src_smaller_than_len_dst():
 def test_sync_li2__len_src_is_same_to_len_dst():
     assert sync_li2([1,2], [1,2], modify_left=True, padval=0) == [1,2]
     assert sync_li2([],    [],    modify_left=True, padval=0) == []
+
