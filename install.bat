00d2f27c606bb8efa704aef19590a6cb48d432a8
KUR-creative <rhdnfka94@gmail.com>
2019-09-23 18:17:25 +0900

    Add windows install script

---
+++ install.bat
@@ -0,0 +1,8 @@
+python -m venv venv
+cd venv\Scripts
+call .\activate
+cd ..\..
+pip install tqdm funcy pygments networkx
+pip install markupsafe toolz 
+pip install sidekick
+pip install hyperpython
